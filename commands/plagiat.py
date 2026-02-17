1bce7f9541bf7d6f5081a826446247b4bae93b97
Miloctober <dianaberthod14@gmail.com>
2026-02-17 20:01:39 +0100

    création de plagiat.py, je le compléterais

--- commands/plagiat.py
+++ commands/plagiat.py
@@ -1,1 +1,12 @@
 import discord
+"""Cette commande permet de:
+https://discordpy.readthedocs.io/en/stable/api.html?highlight=text#discord.TextChannel.history 
+mdr oops
+- euhhh oui donc ça permet en fait de soit envoyer de façon random un message de n'importe quel 
+    channel de n'importe quel utilisateur etc., mais on peut ajouter des paramètres pour le fun
+- paramètres référençables: Nombre de messages (Si y'a pas de numéro de message spécifique), Utilisateur, 
+    Channel, Numéro de message spécifique
+
+Ca ajoutera un max de fun dans nos conversations, faire attention aux spams par contre!
+    
+"""
